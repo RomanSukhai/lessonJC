26fb1d1900bf0353b8e324dd79baf41f0374ef7e
Roman Sukhai <romansukhai@gmail.com>
2021-08-04 00:07:55 +0300

    Initial commit

---
+++ src/ua/lviv/lgs/Application.java
@@ -0,0 +1,130 @@
+
+package ua.lviv.lgs;
+
+public class Application {
+    public static void main(String[] args) {
+
+//        int a = 483;
+//        short b = 494;                /
+//        float c = 3.90f;              | ---- Перше завдання
+//        long e = 493950935909350l;    \
+//        double f =4.3;
+//        boolean g = false;
+//        char r = 's';
+
+
+
+
+//        Byte g = 3;
+//        Long v = 542l;
+//        Short e = 9303;                           /
+////                                                | ---- Друге завдання
+////                                                \
+//
+//        System.out.println(Byte.MAX_VALUE);
+//        System.out.println(Long.MIN_VALUE);
+//        System.out.println(Short.MAX_VALUE);
+
+
+
+
+
+
+
+
+//        double[] a = new double[11];
+//        a[0] = 64;
+//        a[1] = 57;
+//        a[2] = 43;
+//        a[3] = 21;
+//        a[4] = 64;
+//        a[5] = 543;
+//        a[6] = 12;
+//        a[7] = 2;
+//        a[8] = 313;
+//        a[9] = 404;
+//        a[10] = 156;
+//
+//        double minValue = a[0];                                       /
+//        int minIndex =0;                                              | --- Мінімальне значення масиву
+//                                                                      \
+//
+//
+//        for (int i = 1 ;i <a.length;i++){
+//            if (a[i]<minValue){
+//                minValue = a[i];
+//                minIndex = i;
+//            }
+//        }
+//
+//
+//        System.out.println("min value:" + minValue);
+//        System.out.println("min index:" + minIndex);
+
+
+
+
+
+
+
+//        double[] a = new double[11];
+//        a[0] = 64;
+//        a[1] = 57;
+//        a[2] = 43;
+//        a[3] = 21;
+//        a[4] = 64;
+//        a[5] = 543;
+//        a[6] = 12;
+//        a[7] = 2;
+//        a[8] = 313;
+//        a[9] = 404;
+//        a[10] = 156;
+//
+//        double maxValue = a[0];
+//        int maxIndex =0;
+//
+//
+//
+//        for (int i = 1 ;i <a.length;i++){                                             /
+//            if (a[i]>maxValue){                                                       | --- Максимальне значення масиву
+//                maxValue = a[i];                                                      \
+//                maxIndex = i;
+//            }
+//        }
+//
+//
+//        System.out.println("min value:" + maxValue);
+//        System.out.println("min index:" + maxIndex);
+
+
+        int a =12;
+        int b = 13;
+        int c = 78;
+
+        boolean f = false;
+        while(a<=200){
+            System.out.println("Цикл a: "+a);
+            a+=2;
+            System.out.println(" ");
+
+            if (a>=150){
+
+                System.out.println("Ще трошки.....");
+
+
+            }
+             if(a==200){
+                System.out.println("END");
+            }
+
+            }
+
+
+        }
+    }
+
+
+
+
+
+
